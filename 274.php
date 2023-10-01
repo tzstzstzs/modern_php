babcd1acb28828a1394429dea8f9e6ef6de844f7
tzstzstzs <tarr.zsolt.mailbox@gmail.com>
2023-10-01 21:20:42 +0200

    first

---
+++ 274.php
@@ -0,0 +1,11 @@
+<?php
+$data = [
+    'question' => 'Melyik a kedvenc frontend JavaScript keretrendszered',
+    'answers' => [
+        'Vue' => 0,
+        'React' => 0,
+        'Svelte' => 0,
+        'Angular' => 0,
+        'jQuery' => 0,
+    ]
+];
