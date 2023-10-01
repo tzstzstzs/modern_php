fc82c4982a673db30e4400b3644a8c4e638251e6
tzstzstzs <tarr.zsolt.mailbox@gmail.com>
2023-10-01 21:51:14 +0200

    1. videó 33:00

--- 274.php
+++ 274.php
@@ -2,10 +2,10 @@
 $data = [
     'question' => 'Melyik a kedvenc frontend JavaScript keretrendszered',
     'answers' => [
-        'Vue' => 0,
-        'React' => 0,
-        'Svelte' => 0,
-        'Angular' => 0,
-        'jQuery' => 0,
+        'Vue' => 10,
+        'React' => 3,
+        'Svelte' => 1,
+        'Angular' => 2,
+        'jQuery' => 4,
     ]
 ];
