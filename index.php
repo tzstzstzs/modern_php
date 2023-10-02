393a6b0a7bd1c8c4bf568c1928d6e838aa6e81a5
tzstzstzs <tarr.zsolt.mailbox@gmail.com>
2023-10-02 21:10:33 +0200

    second

--- index.php
+++ index.php
@@ -4,4 +4,13 @@ $dayOfYear = 274;
 require './' . $dayOfYear . '.php';
 $totalVotes = array_sum($data['answers']);
 
+if ($_POST['vote']) {
+    if (in_array($_POST['vote'], array_keys($data['answers']))) {
+        // növeljük
+        $data['answers'][$_POST['vote']]++;
+        var_dump($data);
+    }
+    // TODO ha olyanra szavazott ami nem létezik, logoljuk a választ és az IP címet
+}
+
 require './template.php';
